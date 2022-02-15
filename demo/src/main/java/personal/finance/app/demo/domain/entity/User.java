75adc18f1b8b4a48a24ecb2214c9b239bd38e064
piotr-jadczak <58400623+piotr-jadczak@users.noreply.github.com>
2022-02-16 00:03:08 +0100

    add user

--- demo/src/main/java/personal/finance/app/demo/domain/entity/User.java
+++ demo/src/main/java/personal/finance/app/demo/domain/entity/User.java
@@ -1,4 +1,62 @@
 package personal.finance.app.demo.domain.entity;
 
-public class User {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+    private boolean accountNonLocked;
+    private boolean enabled;
+
+    @OneToOne
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.getName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {return accountNonLocked; }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
